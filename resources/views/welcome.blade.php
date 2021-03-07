18b82562873c18d61a658c094438e1a68ad6f9cc
João Virgilio <jvirgiliomartins09@gmail.com>
2021-03-07 19:44:22 -0300

    Style-home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,31 @@
 
 @section('content')
 
-<h1>Titulo</h1>
+<div id="busca-container" class="col-md-12">
+	<h1>Busque um Evento</h1>
+	<form action="">
+		<input type="text" id="busca" name="busca" class="form-control"  placeholder="Procurar...">
+	</form>
+</div>
+
+<div id="events-container" class="col-md-12">
+	<h2>Próximos eventos</h2>
+	<p>Vejá os eventos dos próximos dias..</p>
+	
+	<div id="cards-container" class="row">
+		@foreach($events as $event)
+		<div class="card col-md-3">
+			<img src="/img/cards-banner.jpg" alt="{{$event->titulo}}">
+			<div class="card-body">
+				<p class="card-date">07/09/2021</p>
+				<h5 class="card-title">{{$event->titulo}}</h5>
+				<p class="card-participants">X Participantes</p>
+				<a href="#" class="btn btn-primary">Saiba mais</a>
+			</div>
+		</div>
+		@endforeach
+
+	</div>
+</div>
 
 @endsection
