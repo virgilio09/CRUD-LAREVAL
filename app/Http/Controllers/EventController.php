d8329f5e1052cc9c0c2fc51bfda86e3a6c697536
João Virgilio <jvirgiliomartins09@gmail.com>
2021-03-09 11:36:58 -0300

    Ajuste no css

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -31,6 +31,6 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Envento criado com sucesso!!');
     }
 }
